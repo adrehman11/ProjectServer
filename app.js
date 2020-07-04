9b3f4728de026a257f862b781f03a1e1cfe6efd1
Abdul Rehman Sohail <adrehman11@gmail.com>
2020-07-04 04:27:36 -0700

    host added

--- app.js
+++ app.js
@@ -149,6 +149,7 @@ app.use(function(err, req, res, next) {
 
 const socketOps = require("./socketOps/socketOps.js");
 socketOps.allSocketOps(io);
-server.listen(port,()=> console.log("server listening on 3000 port"))
+const host = '0.0.0.0';
+server.listen(port,host,()=> console.log("server listening on 3000 port"))
 
 module.exports = app;
