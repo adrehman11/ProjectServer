9aec09ff9fbb7db33e2f3ea3dbbb53cbdf56eed6
Abdul Rehman Sohail <adrehman11@gmail.com>
2020-07-05 03:11:37 -0700

    erroe 202

--- routes/order2.js
+++ routes/order2.js
@@ -199,7 +199,7 @@ router.post('/myorder/pending', (req, res) => {
     var resdata2=[];
 
     if (utype == 'Customer') {
-        console.log(user_ID)
+  
         orders.find({ $and: [ { userID:user_ID }, { ratingStatus:"NotDone" },{orderStatus:"Finish"} ] },async function(err,loi){
             if(err)
             {
@@ -229,7 +229,7 @@ router.post('/myorder/pending', (req, res) => {
                     resdata2.push(datp)
                 }
                 console.log(resdata2);
-               await orders.find({ userID: user_ID } && { orderStatus: status }, async function (error, data12) {
+               await orders.find({ userID: user_ID,orderStatus: status }, async function (error, data12) {
                     if (error) {
                         console.log(error);
                     }
